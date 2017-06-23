7f8b892cf973fe25d72f00381d112e32f422e590
nbiton <naor.biton@gmail.com>
2017-06-23 11:26:18 +0800

    simple boilerplate code

---
+++ imports/ui/app.jsx.js
@@ -0,0 +1,63 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+
+import Task from './task.jsx';
+
+// App component - represents the whole app
+class App extends Component {
+	renderTasks() {
+		return this.props.tasks.map((task) => (
+			<Task key={task._id} task={task} />
+		));
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		// Find the text field via the React ref
+		const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(), // current time
+		});
+
+		// Clear form
+		ReactDOM.findDOMNode(this.refs.textInput).value = '';
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<header>
+					<h1>Todo List</h1>
+
+					<form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+						<input
+							type="text"
+							ref="textInput"
+							placeholder="Type to add new tasks"
+						/>
+					</form>
+				</header>
+
+				<ul>
+					{this.renderTasks()}
+				</ul>
+			</div>
+		);
+	}
+}
+
+App.propTypes = {
+	tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+	return {
+		tasks: Tasks.find({},  { sort: { createdAt: -1 } }).fetch(),
+	};
+}, App);
