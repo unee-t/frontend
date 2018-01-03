a87625422798ad0745c0ccacce075319daa77d65
Kai Hendry <hendry@iki.fi>
2018-01-03 15:20:39 +0800

    chore: Re-factored deployment script (#52)
    * Check again if AWS_PROFILE is setup. If not, deploy script probably
    needs some special env coaxing
    
    * Re-factor deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ STAGE=dev
 # Usage info
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-p] COMMIT
+Usage: ${0##*/} [-p]
 
 By default, deploy to dev environment on AWS account 8126-4485-3088
 
@@ -30,20 +30,29 @@ done
 AWS_PROFILE=lmb-$STAGE
 shift "$((OPTIND-1))"   # Discard the options and sentinel --
 
-if ! test "$1"
-then
-	show_help >&2
-	exit 1
-fi
-echo COMMIT: $1
+export COMMIT=$(git describe --always)
 
 if ! aws configure --profile $AWS_PROFILE list
 then
 	echo Profile $AWS_PROFILE does not exist >&2
+
+	if ! test "$AWS_ACCESS_KEY_ID"
+	then
+		echo Missing $AWS_ACCESS_KEY_ID >&2
+		exit 1
+	fi
+
 	echo Attempting to setup one from the environment >&2
-	aws configure set profile.lmb-dev.aws_access_key_id $AWS_ACCESS_KEY_ID
-	aws configure set profile.lmb-dev.aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-	aws configure set profile.lmb-dev.region ap-southeast-1
+	aws configure set profile.lmb-${STAGE}.aws_access_key_id $AWS_ACCESS_KEY_ID
+	aws configure set profile.lmb-${STAGE}.aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+	aws configure set profile.lmb-${STAGE}.region ap-southeast-1
+
+	if ! aws configure --profile $AWS_PROFILE list
+	then
+		echo Profile $AWS_PROFILE does not exist >&2
+		exit 1
+	fi
+
 fi
 
 if ! aws configure --profile $AWS_PROFILE list
@@ -56,6 +65,7 @@ if ! hash ecs-cli
 then
 	echo Please install https://github.com/aws/amazon-ecs-cli and ensure it is in your \$PATH
 	echo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest && chmod +x /usr/local/bin/ecs-cli
+	exit 1
 else
 	ecs-cli -version
 fi
@@ -63,8 +73,11 @@ fi
 ecs-cli configure --cluster master --region ap-southeast-1 --compose-service-name-prefix ecscompose-service-
 test -f aws-env.$STAGE && source aws-env.$STAGE
 
-COMMIT=$1 envsubst < AWS-docker-compose-meteor.yml > docker-compose-meteor.yml
+service=$(grep -A1 services AWS-docker-compose.yml | tail -n1 | tr -cd '[[:alnum:]]')
+echo Deploying $service with commit $COMMIT >&2
 
-ecs-cli compose --aws-profile $AWS_PROFILE -p meteor -f docker-compose-meteor.yml service up --timeout 7
+envsubst < AWS-docker-compose.yml > docker-compose-${service}.yml
 
-ecs-cli compose --aws-profile $AWS_PROFILE -p meteor -f docker-compose-meteor.yml service ps
+ecs-cli compose --aws-profile $AWS_PROFILE -p ${service} -f docker-compose-${service}.yml service up --timeout 7
+
+ecs-cli compose --aws-profile $AWS_PROFILE -p ${service} -f docker-compose-${service}.yml service ps
