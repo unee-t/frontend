35b1e7c7dcb15704c8bc1a38da2f84b3beea0578
Naor Biton <naor.biton@gmail.com>
2018-01-08 14:04:27 +0800

    feat: Injecting public meteor settings from env vars (#57)
    * chore: initialization of public Meteor settings in main server file
    
    * chore: removed references to the METEOR_SETTINGS var from ecs deployment
    
    * feat: updated env vars, made them load automatically, injected "public"

--- server/main.js
+++ server/main.js
@@ -5,6 +5,12 @@ import '../imports/api/comments'
 import '../imports/api/hooks/on-create-user'
 import '../imports/api/rest/rest-routes'
 
+const publicClientEnvVars = ['CLOUDINARY_URL', 'CLOUDINARY_PRESET']
+Object.assign(Meteor.settings.public, publicClientEnvVars.reduce((obj, key) => {
+  obj[key] = process.env[key]
+  return obj
+}, {}))
+
 Meteor.startup(() => {
   // code to run on server at startup
 })
