cf62cf385adfe5f81e2ddd3277c6f0bb776cd9a5
Sebastiaan Deckers <sebdeckers83@gmail.com>
2018-06-08 16:18:11 +0800

    fix: use non-deprecated bodyParser API

--- imports/api/rest/rest-routes.js
+++ imports/api/rest/rest-routes.js
@@ -1,12 +1,12 @@
 /* global JsonRoutes */
-import bodyParser from 'body-parser'
+import { json } from 'body-parser'
 // import invitationCreateRoute from './invitation-create'
 import getPendingInvitations from './get-pending-invitations'
 import putPendingInvitations from './put-pending-invitations'
 import getInvitations from './get-invitations'
 import getConvertedInvitations from './get-converted-invitations'
 
-JsonRoutes.Middleware.use(bodyParser())
+JsonRoutes.Middleware.use(json())
 JsonRoutes.Middleware.use((req, res, next) => {
   // The response objects are missing the traditional "send" method by default, so it has to be created artificially
   res.send = (code, data, headers = {}) => {
