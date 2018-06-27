76ad11adb0a129b8996e4c1a049eb9a7e1359740
nbiton <naor.biton@gmail.com>
2018-06-27 17:12:52 +0800

    feat: removing isLimited from a user on pw reset (a bit unrelated)

--- imports/api/hooks/on-login.js
+++ imports/api/hooks/on-login.js
@@ -1,11 +1,18 @@
 import { Accounts } from 'meteor/accounts-base'
+import { Meteor } from 'meteor/meteor'
 import randToken from 'rand-token'
 
 // Enforces one-time usage of random generated passwords for newly invited users
 Accounts.onLogin(info => {
-  if (info.type === 'password') {
+  const { user } = info
+  if (info.methodName === 'resetPassword') {
+    Meteor.users.update(user._id, {
+      $set: {
+        'profile.isLimited': false
+      }
+    })
+  } else if (info.type === 'password') {
     console.log('info', info)
-    const { user } = info
     if (user.profile.isLimited) {
       console.log(`resetting the password for ${user.emails[0].address} after one-time usage by invitation`)
       const randPass = randToken.generate(12)
