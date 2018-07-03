fba191898a621d63938e7238086569a58f1d07c6
Kai Hendry <hendry@iki.fi>
2018-07-03 21:07:11 +0800

    Deprecate https://unee-t.com/contact-support/ (#349)
    * Remove forum/faq link in favour of helpscout support mailto prefilled with support information
    
    * Move template into own variable
    
    * Put back FAQ

--- imports/ui/side-menu/side-menu.jsx
+++ imports/ui/side-menu/side-menu.jsx
@@ -38,6 +38,22 @@ class SideMenu extends Component {
 
   render () {
     const { user, isDrawerOpen, dispatch } = this.props
+    const supportEmailBody = user && `What were you trying to do?
+
+### PLEASE FILL IN ###
+
+What happened?
+
+### PLEASE FILL IN ###
+
+What should have happened?
+
+### PLEASE FILL IN ###
+
+Please insert any screenshots or error messages that might help us! 🙏
+
+DEBUG INFO:
+user: ${user.emails[0].address}`
     return user ? (
       <Drawer
         docked={false}
@@ -66,15 +82,9 @@ class SideMenu extends Component {
         })}
         <Divider />
         {this.linkDrawerItem({
-          href: 'https://unee-t.com/contact-support/',
+          href: 'mailto:support@unee-t.com?subject=' + window.location.href + '&body=' + encodeURIComponent(supportEmailBody),
           iconName: 'live_help',
           text: 'Support',
-          isExternal: true
-        })}
-        {this.linkDrawerItem({
-          href: 'https://forum.unee-t.com/',
-          iconName: 'forum',
-          text: 'Forum',
           isExternal: true
         })}
         {this.linkDrawerItem({
