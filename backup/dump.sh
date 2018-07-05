0f128b0195c6727f54a0b3a8a64b5733a21c3a61
Kai Hendry <hendry@iki.fi>
2018-07-05 10:40:58 +0800

    Correct backup script to point to meteor db (#347)

--- backup/dump.sh
+++ backup/dump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for STAGE in demo
+for STAGE in prod
 do
 	echo Mongo dumping stage $STAGE
 	source ../aws-env.$STAGE
-	mongodump --host $(echo $MONGO_CONNECT | cut -d "/" -f1) --ssl --username root --password $MONGO_PASSWORD --authenticationDatabase admin --db test -o $(date "+$STAGE-%Y%m%d")
+	mongodump --host $(echo $MONGO_CONNECT | cut -d "/" -f1) --ssl --username root --password $MONGO_PASSWORD --authenticationDatabase admin --db meteor -o $(date "+$STAGE-%Y%m%d")
 done
