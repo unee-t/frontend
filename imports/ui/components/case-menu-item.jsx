fc751b8a72091d8d941f6927408fe726b644944c
Naor Biton <naor.biton@gmail.com>
2018-10-30 18:57:19 +0800

    feat: showing closed cases as closed in the unit's case list #623 (#625)
    Tested in Heroku - works as intended

--- imports/ui/components/case-menu-item.jsx
+++ imports/ui/components/case-menu-item.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import MenuItem from 'material-ui/MenuItem'
 import FontIcon from 'material-ui/FontIcon'
 
+import { isClosed } from '/imports/api/cases'
+
 const severityIcons = {
   'DEAL BREAKER!': {
     icon: 'format_align_justify',
@@ -32,19 +34,26 @@ const menuItemDivStyle = {
   padding: 0
 }
 
-const CaseMenuItem = ({ caseItem: { severity, title }, onClick, className = '' }) => (
-  <div className={'bb b--very-light-gray bg-white ' + className}>
-    <MenuItem
-      innerDivStyle={menuItemDivStyle}
-      onClick={onClick}
-    >
-      <FontIcon className='material-icons mr2' color={severityIcons[severity].color}>
-        {severityIcons[severity].icon}
-      </FontIcon>
-      <span className='bondi-blue pl1 ellipsis'>{title}</span>
-    </MenuItem>
-  </div>
-)
+const CaseMenuItem = ({ caseItem, onClick, className = '' }) => {
+  const { severity, title } = caseItem
+  return (
+    <div className={'bb b--very-light-gray bg-white ' + className}>
+      <MenuItem
+        innerDivStyle={menuItemDivStyle}
+        onClick={onClick}
+      >
+        <FontIcon className='material-icons mr2' color={severityIcons[severity].color}>
+          {severityIcons[severity].icon}
+        </FontIcon>
+        <span
+          className={'pl1 ellipsis ' + (isClosed(caseItem) ? 'silver strike' : 'bondi-blue')}
+        >
+          {title}
+        </span>
+      </MenuItem>
+    </div>
+  )
+}
 
 CaseMenuItem.propTypes = {
   caseItem: PropTypes.object.isRequired,
