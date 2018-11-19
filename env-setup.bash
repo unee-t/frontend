2cfbc902651a2de942bb3cde5e6df2606ce3ebaa
Kai Hendry <hendry@iki.fi>
2018-11-19 14:35:17 +0800

    Helper to set developers using https://dashboard.dev.unee-t.com/ as their backend (#638)

--- env-setup.bash
+++ env-setup.bash
@@ -10,6 +10,14 @@ ssm() {
 	echo $(aws --profile uneet-dev ssm get-parameters --names $1 --with-decryption --query Parameters[0].Value --output text)
 }
 
+setuplocal() {
+cat << EOF >> .env
+APIENROLL_LAMBDA_URL=http://localhost:4000
+INVITE_LAMBDA_URL=http://localhost:9000
+UNIT_CREATE_LAMBDA_URL=http://localhost:4001/create
+EOF
+}
+
 cat << EOF > .env
 BUGZILLA_ADMIN_KEY=$(ssm BUGZILLA_ADMIN_KEY)
 MAIL_URL=smtps://$(ssm SES_SMTP_USERNAME):$(ssm SES_SMTP_PASSWORD)@email-smtp.us-west-2.amazonaws.com:465
@@ -17,11 +25,28 @@ CLOUDINARY_URL=https://api.cloudinary.com/v1_1/unee-t-staging/image/upload
 CLOUDINARY_PRESET=$(ssm CLOUDINARY_PRESET)
 API_ACCESS_TOKEN=$(ssm API_ACCESS_TOKEN)
 FROM_EMAIL="Local Unee-T Case <case.local@unee-t.com>"
-APIENROLL_LAMBDA_URL=http://localhost:4000
-INVITE_LAMBDA_URL=http://localhost:9000
-UNIT_CREATE_LAMBDA_URL=http://localhost:4001/create
 STAGE=dev
 DOMAIN=unee-t.com
 PDFGEN_LAMBDA_URL=https://pdfgen.dev.unee-t.com
 PDFCONVERT_LAMBDA_URL=https://prince.dev.unee-t.com
 EOF
+
+if ! curl -s localhost:8081
+then
+read -p "https://github.com/unee-t/bugzilla-customisation not running, use https://dashboard.dev.unee-t.com/ ?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+cat << EOF >> .env
+APIENROLL_LAMBDA_URL=https://apienroll.dev.unee-t.com
+INVITE_LAMBDA_URL=https://invite.dev.unee-t.com
+UNIT_CREATE_LAMBDA_URL=https://unit.dev.unee-t.com/create
+BUGZILLA_URL=https://dashboard.dev.unee-t.com/
+EOF
+else
+echo Setting up for local https://github.com/unee-t/bugzilla-customisation
+setuplocal
+fi
+else
+setuplocal
+fi
