ca1d54d8a342463b05b1b387e2a865d7b1157126
Naor Biton <naor.biton@gmail.com>
2019-08-12 20:54:49 +0800

    feat: file selection to show all files but show error if type invalid (#860)

--- imports/ui/components/file-input.jsx
+++ imports/ui/components/file-input.jsx
@@ -1,6 +1,7 @@
 // @flow
 /* global SyntheticInputEvent, HTMLInputElement */
 import * as React from 'react'
+import ErrorDialog from '../dialogs/error-dialog'
 
 type Props = {
   children: React.Node,
@@ -11,21 +12,43 @@ type Props = {
   onFileSelected: (evt: SyntheticInputEvent<HTMLInputElement>) => void
 }
 
-export default class FileInput extends React.Component<Props> {
-  render () {
-    const acceptTypes = this.props.acceptTypes || { image: true, audio: false }
-    let acceptStrs = []
-    if (acceptTypes) {
-      acceptStrs.push('audio/*')
-    }
-    if (acceptTypes.image) {
-      acceptStrs.push('image/*')
-    }
+type State = {
+  typeError: boolean
+}
 
+export default class FileInput extends React.Component<Props, State> {
+  state = {
+    typeError: false
+  }
+  handleFileSelected = (evt: SyntheticInputEvent<HTMLInputElement>) => {
+    const fileType = evt.target.files[0].type.split('/')[0]
+    if (this.allowedTypes.includes(fileType)) {
+      this.props.onFileSelected(evt)
+    } else {
+      evt.preventDefault()
+      this.setState({
+        typeError: true
+      })
+    }
+  }
+
+  get allowedTypes ():Array<string> {
+    const acceptTypes = this.props.acceptTypes || { image: true, audio: false }
+    return Object.keys(acceptTypes).filter(type => !!acceptTypes[type])
+  }
+
+  render () {
     return (
       <label>
         {this.props.children}
-        <input type='file' accept={acceptStrs.join(', ')} capture='filesystem' className='dn' onChange={this.props.onFileSelected} />
+        <input type='file' className='dn' onChange={this.handleFileSelected} />
+        <ErrorDialog
+          show={this.state.typeError}
+          text={`The file type you selected is not supported for this input. The supported type${
+            this.allowedTypes.length === 1 ? ` is "${this.allowedTypes[0]}"` : `s are "${this.allowedTypes.join('", "')}"`
+          }`}
+          onDismissed={() => this.setState({ typeError: false })}
+        />
       </label>
     )
   }
