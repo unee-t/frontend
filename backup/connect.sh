18e24589d2b21b7e3257b3ce6f9260fe7ab7d7d4
Kai Hendry <kai.hendry@unee-t.com>
2019-12-29 19:19:35 +0800

    Update mongo connect for Mongo 4.0 DB

--- backup/connect.sh
+++ backup/connect.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-
 STAGE=dev
 
 show_help() {
 cat << EOF
 Usage: ${0##*/} [-p]
 
-By default, deploy to dev environment on AWS account 812644853088
+By default, deploy to dev environment on AWS account
 
-	-p          PRODUCTION 192458993663
-	-d          DEMO 915001051872
+	-p          PRODUCTION
+	-d          DEMO
 
 EOF
 }
@@ -32,9 +31,11 @@ do
 			;;
 	esac
 done
-AWS_PROFILE=uneet-$STAGE
+AWS_PROFILE=ins-$STAGE
 shift "$((OPTIND-1))"   # Discard the options and sentinel --
 
-MONGO_PASSWORD=$(aws --profile $AWS_PROFILE ssm get-parameters --names MONGO_PASSWORD --with-decryption --query Parameters[0].Value --output text)
-MONGO_CONNECT=$(aws --profile $AWS_PROFILE ssm get-parameters --names MONGO_CONNECT --query Parameters[0].Value --output text)
-mongo "mongodb://root:$MONGO_PASSWORD@$MONGO_CONNECT"
+getparam () {
+	aws --profile ins-${STAGE} ssm get-parameters --names "$1" --with-decryption --query Parameters[0].Value --output text
+}
+
+mongo "mongodb+srv://$(getparam MONGO_MASTER_USERNAME):$(getparam MONGO_MASTER_PASSWORD)@$(getparam MONGO_CONNECT)"
